849871f9d9c1708071cc6f002a765fd5b5287d13
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2021-02-26 16:51:02 -0500

    Add http request to get list of stored posts

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,31 +1,26 @@
 import styled from 'styled-components';
 
 
+
 const MainContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
   padding: 2.5rem;
-  width: 60rem;
+  width: 40rem;
   border: 2px solid #706c61;
   background-color: #e1f4f3;
   color: #706c61;
 `;
 const P = styled.p`
-
   font-size: 1.2rem;
   padding: 1rem;
   text-align: left;
-
 `;
-function Content({ content }) {
+function Content({ chosenContent }) {
+  const { question, answer, link } = chosenContent;
   return (
     <MainContent>
-      <h1>Answers</h1>
-      <h2>{ content.question }</h2>
-      <P>{ content.answer }</P>
-      <a href={ content.link }> Recommended Documentation </a>
+      <h2>{ question }</h2>
+      <P>{ answer }</P>
+      <a href={ link }> Recommended Documentation </a>
 
     </MainContent>
   );
